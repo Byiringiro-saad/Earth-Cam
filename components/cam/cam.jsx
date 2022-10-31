766e1198d340ad7000c9dba7edbcbb7073c1a4b1
mrraghur <iamanonymouscoward@gmail.com>
2022-10-31 12:12:55 -0700

    When app loads for first time, make a call to backend and get list of all pics. Display the same on webpage.

--- components/cam/cam.jsx
+++ components/cam/cam.jsx
@@ -6,8 +6,10 @@ import { AiFillEye } from "react-icons/ai";
 
 import styles from "./cam.module.css";
 
-const Cam = ({ datas }) => {
+const Cam = ({ datas, title, width, url }) => {
   const router = useRouter();
+  const router = useRouter();
+  //const url = `https://picsum.photos/300/300?random=${Math.random()}`;
 
   const goToCam = () => {
     router.push(`/${datas?.title.toLowerCase()}/5`);
@@ -30,7 +32,8 @@ const Cam = ({ datas }) => {
       onClick={goToCam}
     >
       <div className={styles.image}>
-        <img src={datas?.imageUrl} alt="banner" />
+        <img src={url} alt="banner" />
+        {/* <img src="https://random.imagecdn.app/500/500" alt="sasda" /> */}
       </div>
       <div className={styles.about}>
         <p className={styles.title}>Tidal Basin Cam</p>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 
+import axios from "axios";
+
 import Nav from "../components/nav/nav";
 import Cams from "../components/cams/cams";
 import styles from "../styles/Home.module.css";
@@ -20,13 +22,52 @@ export default function Home() {
     "Sports",
   ]);
 
+ const [listPicsMetadata, setListPicsMetadata]  = useState([]);
+
+  useEffect(() => {
+    axios.get("https://backend.interviewblindspots.com/displaycode/listpics/")
+        .then (res => {
+            console.log("my pics is");
+            console.log (res)
+            console.log (res?.data?.pics)
+            setListPicsMetadata(res?.data?.pics);
+        }).catch (err => {
+            console.log(err)
+        })
+    }, [])//(, [dispatch])
+
+    var urls = [];
+    //var urls = ["https://url1.com/xyz.png", "https://url2.com/abc.png"];
+    //urls[index % 2]
+    //start from some random number, for now
+    var min = 10;
+    var max = 1000;
+    var startIndex = Math.floor(Math.random() * (max - min + 1) + min);
+    var howManyPics = 5;
+    for (var i=startIndex; i<startIndex+howManyPics; i++) {
+        //Eg: https://backend.interviewblindspots.com/displaycode/pic/output703.jpg",
+        var currUrl = "https://backend.interviewblindspots.com/displaycode/pic/output"+i+".jpg";
+        urls.push(currUrl);
+    }  
+
   return (
     <div className={styles.container}>
       <Nav />
       <Top />
+
+      <div className={styles.cams}>
+        {listPicsMetadata.map((picMetadata, index) => (
+          <Cams key={index} title={picMetadata.name} num={index} url={picMetadata.url} />
+        ))}
+      </div>
+
+
+
+
+
       <div className={styles.cams}>
         {cams.map((cam, index) => (
-          <Cams key={index} title={cam} num={index} />
+          <Cams key={index} title={cam} num={index} url={urls[index]} urlArray={urls}/>
         ))}
       </div>
       <PaginationC />
